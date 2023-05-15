1ec293f5965742831dc4bf9ae707a7e000b4971c
Karl Beier <karl.beier1@gmail.com>
2023-05-15 14:36:36 +1000

    Fix issues in power priority

--- power-priority.lua
+++ power-priority.lua
@@ -1,10 +1,10 @@
-baseEnergyStorage = peripheral.wrap("down")
+baseEnergyStorage = peripheral.wrap("bottom")
 spsEnergyStorageDirection = "left"
 
 while (true) do
 	if (baseEnergyStorage.getEnergyFilledPercentage() >= 0.9) then
-		redstone.setOutput(spsEnergyStorageDirection, true)
-	else
 		redstone.setOutput(spsEnergyStorageDirection, false)
+	else
+		redstone.setOutput(spsEnergyStorageDirection, true)
 	end
 end
