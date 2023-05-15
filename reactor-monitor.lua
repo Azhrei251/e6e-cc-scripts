e41be66c26a9f8e6a77e709ad2b7eb2b7cf4648f
Azhrei251 <karl.beier1@gmail.com>
2023-05-15 10:03:09 +1000

    Update reactor-monitor.lua

--- reactor-monitor.lua
+++ reactor-monitor.lua
@@ -1,4 +1,5 @@
 PROTOCOL = "reactor"
+RESET_PROTOCOL = "reactor-reset"
 
 reactor = peripheral.wrap("back")
 monitor = peripheral.wrap("right")
@@ -9,31 +10,36 @@ monitor.setTextScale(0.5)
 print("Monitoring reactor") 
 while (true) do
 	monitor.clear()
-	coolantPercent = reactor.getCoolantFilledPercentage()
-	wastePercent = reactor.getWasteFilledPercentage()
-	fuelPercent = reactor.getFuelFilledPercentage()
-	burnRate = reactor.getBurnRate()
-	reactorEnabled = reactor.getStatus()
+	
+	if (reactor.isFormed()) then
+		coolantPercent = reactor.getCoolantFilledPercentage()
+		wastePercent = reactor.getWasteFilledPercentage()
+		fuelPercent = reactor.getFuelFilledPercentage()
+		burnRate = reactor.getBurnRate()
+		reactorEnabled = reactor.getStatus()
 
-	if (reactorEnabled and (coolantPercent <= 0.1 or wastePercent >= 0.95)) then
-		reactor.scram()
+		if (reactorEnabled and (coolantPercent <= 0.1 or wastePercent >= 0.95)) then
+			reactor.scram()
+		end
+
+		messageContent = {
+			["coolantPercent"] = coolantPercent,
+			["wastePercent"] = wastePercent,
+			["fuelPercent"] = fuelPercent,
+			["burnRate"] = burnRate,
+			["reactorEnabled"] = reactorEnabled,
+		}
+		rednet.broadcast(messageContent, PROTOCOL)
+
+		line = 1
+		for k,v in pairs(messageContent) do
+			monitor.setCursorPos(1, line)
+			monitor.write(k..": "..tostring(v))
+			line = line + 1
+		end
+
+		sleep(0.05)
+	else
+		rednet.broadcast("reset", RESET_PROTOCOL)
 	end
-	 
-	messageContent = {
-		["coolantPercent"] = coolantPercent,
-		["wastePercent"] = wastePercent,
-		["fuelPercent"] = fuelPercent,
-		["burnRate"] = burnRate,
-		["reactorEnabled"] = reactorEnabled,
-	}
-	rednet.broadcast(messageContent, PROTOCOL)
-	
-	line = 1
-	for k,v in pairs(messageContent) do
-		monitor.setCursorPos(1, line)
-		monitor.write(k..": "..tostring(v))
-		line = line + 1
-	end
-	
-	sleep(0.05)
 end
